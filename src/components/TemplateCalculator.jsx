8b438f07f32d5c5255ca607aaa8f12277b7ea5f2
neil <1522674529@qq.com>
2022-01-01 16:09:06 +0800

    配置eslint

--- src/components/TemplateCalculator.jsx
+++ src/components/TemplateCalculator.jsx
@@ -1,46 +1,42 @@
-import React from 'react'
+import React from 'react';
 
 const scaleNames = {
-    c: 'Celsius',
-    f: 'Fahrenheit'
+  c: 'Celsius',
+  f: 'Fahrenheit'
 };
 
 class TemperatureInput extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleChange = this.handleChange.bind(this);
-        this.state = { temperature: '' };
-    }
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = { temperature: '' };
+  }
 
-    handleChange(e) {
-        this.setState({ temperature: e.target.value });
-    }
+  handleChange(e) {
+    this.setState({ temperature: e.target.value });
+  }
 
-    render() {
-        const temperature = this.state.temperature;
-        const scale = this.props.scale;
-        return (
-            <fieldset>
-                <legend>Enter temperature in {scaleNames[scale]}:</legend>
-                <input value={temperature}
-                    onChange={this.handleChange} />
-            </fieldset>
-        );
-    }
+  render() {
+    const temperature = this.state.temperature;
+    const scale = this.props.scale;
+    return (
+      <fieldset>
+        <legend>Enter temperature in {scaleNames[scale]}:</legend>
+        <input value={temperature} onChange={this.handleChange} />
+      </fieldset>
+    );
+  }
 }
 
 class Calculator extends React.Component {
-        render() {
-        return (
-            <div>
-                <TemperatureInput scale="c" />
-                <TemperatureInput scale="f" />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <TemperatureInput scale="c" />
+        <TemperatureInput scale="f" />
+      </div>
+    );
+  }
 }
 
-ReactDOM.render(
-    <Calculator />,
-    document.getElementById('root')
-);
+export default Calculator;
