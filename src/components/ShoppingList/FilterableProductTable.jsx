5b915dc2f968a30b6cb890f7176ee9fb3e70d7b9
neil <1522674529@qq.com>
2022-01-02 16:20:45 +0800

    改为用hooks实现

--- src/components/ShoppingList/FilterableProductTable.jsx
+++ src/components/ShoppingList/FilterableProductTable.jsx
@@ -1,63 +1,48 @@
 import React from 'react'
+import { useState } from 'react'
+
 import ProductTable from './ProductTable'
 import SearchBar from './SearchBar'
 import mockData from './mockData'
 import './App.css'
 
-class FilterableProductTable extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      filterText: '',
-      isStockOnly: false
-    }
-    this.handleFilterTextChange = this.handleFilterTextChange.bind(this)
-    this.handleIsStockOnlyChange = this.handleIsStockOnlyChange.bind(this)
-  }
+function getDisplayedItems(filterText, isStockOnly) {
+  let ret = isStockOnly
+    ? mockData.filter((item) => item.stocked === true)
+    : mockData
+  ret = ret.filter((item) => item.name.startsWith(filterText))
+  return ret
+}
 
-  handleFilterTextChange(e) {
-    this.setState({ filterText: e.target.value })
-  }
+function FilterableProductTable() {
+  const [filterText, setFilterText] = useState('')
+  const [isStockOnly, setIsStockOnly] = useState(false)
 
-  handleIsStockOnlyChange(value) {
-    this.setState({ isStockOnly: value })
-  }
-
-  getDisplayedItems() {
-    let ret = this.state.isStockOnly
-      ? mockData.filter((item) => item.stocked === true)
-      : mockData
-    ret = ret.filter((item) => item.name.startsWith(this.state.filterText))
-    return ret
-  }
-
-  render() {
-    const displayedItems = this.getDisplayedItems()
-    return (
-      <div className="external">
-        <SearchBar
-          filterText={this.state.filterText}
-          isStockOnly={this.state.isStockOnly}
-          handleFilterTextChange={this.handleFilterTextChange}
-          handleIsStockOnlyChange={this.handleIsStockOnlyChange}
+  const displayedItems = getDisplayedItems(filterText, isStockOnly)
+  return (
+    <div className="external">
+      <SearchBar
+        filterText={filterText}
+        isStockOnly={isStockOnly}
+        handleFilterTextChange={(e) => setFilterText(e.target.value)}
+        handleIsStockOnlyChange={(value) => setIsStockOnly(value)}
+      />
+      <div className="productTable">
+        <ProductTable
+          title="Sporting Goods"
+          dataSource={displayedItems.filter(
+            (item) => item.category === 'Sporting Goods'
+          )}
         />
-        <div className="productTable">
-          <ProductTable
-            title="Sporting Goods"
-            dataSource={displayedItems.filter(
-              (item) => item.category === 'Sporting Goods'
-            )}
-          />
-          <ProductTable
-            title="Electronics"
-            dataSource={displayedItems.filter(
-              (item) => item.category === 'Electronics'
-            )}
-          />
-        </div>
+        <ProductTable
+          title="Electronics"
+          dataSource={displayedItems.filter(
+            (item) => item.category === 'Electronics'
+          )}
+        />
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default FilterableProductTable
